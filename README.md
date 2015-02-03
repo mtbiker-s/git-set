f4bdde78eda0306089cb8a9d5a1f48a3ee76730c
Carlo E. Sanchez <carloesanchez@gmail.com>
2015-02-03 10:25:13 -0800

    Made modifications to script for public availability and added the documentation to the README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # git-set
-A bash script that switches your git profile from your enterprise to personal and vice versa
+A bash script that helps with switching your ssh git profile from your enterprise to personal and vice versa. Very helpful if you use git cli.
+
+## First Steps
+In order for these script to work you'll need to setup your ssh keys.
+You can do this by following the steps in the GitHub help for "[Generating SSH keys](https://help.github.com/articles/generating-ssh-keys/)".
+
+Rember to create one rsa file for work aka enterprise and one for private aka personal. 
+
+## Configuring the script
+Change the variables in the section that reads **Make Changes Here**.
+
+
+```
+## Make Changes Here
+## Start
+# Creating the variable for choices
+# These should be changed accordingly to your preference
+work="work"
+workEmail="your_work_email_address"
+workIdRsaFile="/path/to/your/work/.ssh/id_rsa_file"
+# E.g --  workIdRsaFile="/Users/$USER/.ssh/id_work_rsa"
+
+private="private"
+privateEmail="your_personal_email_address"
+privateIdRsaFile="/path/to/your/private/.ssh/id_rsa_file"
+# E.g. -- privateIdRsaFile="/Users/$USER/.ssh/id_private_rsa"
+```
+## Run and Test
+Make sure all of the changes you've made are working accordingly. You can run with "./gitset.sh" 
+
+It will prompt you to enter what ssh profile you want to use then it will promt your for your ssh passcode.
+
+## Using the script
+Run with ./gitset.sh when ever you need to change your GitHub ssh profile or create an alias in your .profile or .bashrc
+
+E.g. alias gitset='bash /path/to/your/gitset.sh' 
--- gitset.sh
+++ gitset.sh
@@ -5,12 +5,26 @@
 curDir=pwd
 # file separator
 fs="/"
-# Creating the vars for choices
-work="work"
-workEmail="you@work.org"
-private="private"
-privateEmail="you@private.org"
 
+## Make Changes here
+## Start
+# Creating the variable for choices
+# These should be changed accordingly to your preference
+work="work"
+workEmail="your_work_email_address"
+workIdRsaFile="/path/to/your/work/.ssh/id_rsa_file"
+# E.g --  workIdRsaFile="/Users/$USER/.ssh/id_work_rsa"
+
+private="private"
+privateEmail="your_personal_email_address"
+privateIdRsaFile="/path/to/your/private/.ssh/id_rsa_file"
+# E.g. -- privateIdRsaFile="/Users/$USER/.ssh/id_private_rsa"
+## End
+
+##
+# Function to switch the git user
+# Takes 1 parameter
+##
 switchGitUser(){
 # Git aliases to changeusers
 gitProfile=$1
@@ -18,19 +32,26 @@ gitProfile=$1
 eval "$(ssh-agent -s)"
 if [[ $gitProfile == $work ]]; then
 	echo "Will use "$gitProfile
-	`ssh-add /home/$USER/.ssh/work_id_rsa`
+	`ssh-add $workIdRsaFile`
 	git config --global user.email "$workEmail"
-	git config -l
+	
 else
 	echo "Will use "$gitProfile
-	`ssh-add /home/$USER/.ssh/personal_id_rsa`
+	`ssh-add $privateIdRsaFile`
 	git config --global user.email "$privateEmail"
-	git config -l
+	
 fi
+
+echo
+echo "Current git configuration:"
+git config -l
 
 
 }
 
+##
+# Function that will process everything
+##
 process(){
 echo 
 echo "Program attempts to switch the git ssh key profile and git user to user with that profile"
@@ -39,13 +60,19 @@ echo "Please enter the name of the git profile you want to use."
 echo "Current choices are '$work' or '$private'"
 read gitProfileToUse
 
+## Call to switchGitUser function
 switchGitUser $gitProfileToUse
 
 }
 
+
+##
+# Function that calls the process. If this function is not called
+# the program won't run
+##
 runScript(){
 process
 }
 
-# if runScript is not called program won't run
+# Calling function runScript so program will run
 runScript
